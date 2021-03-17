6d6316a2c2dd594ca4a19fa29137a382a71bbc08
Victor Velea <47933665+batfolx@users.noreply.github.com>
2021-03-16 21:49:58 -0400

    Add files via upload

--- ESPServo.h
+++ ESPServo.h
@@ -1,6 +1,16 @@
 #include <stdint.h>
 #include "esp32-hal-adc.h"
 
+// Values for TowerPro SG90 small servos; adjust if needed
+#define COUNT_LOW 1638
+#define COUNT_HIGH 7864
+#define COUNT_MID COUNT_HIGH / 2
+#define TIMER_WIDTH 16
+#define HZ_US 50
+#define DELAY 50
+
+
+
 class ESPServo {
   private:
     int8_t pin;
@@ -16,13 +26,31 @@ class ESPServo {
       this->pin = pin;
       this->channel = channel;
       this->hz = hz;
+
+      if ( hz == 0 ) {
+        this->hz = HZ_US;
+      } else {
+        this->hz = hz;
+      }
       if ( timer_width == 0 ) {
         this->timer_width = 16; 
       } else {
         this->timer_width = timer_width;
       }
-      this->low = low;
-      this->high = high;
+
+      if ( low == -1 ) {
+         this->low = COUNT_LOW;
+      } else {
+        this->low = low;  
+      }
+
+      if ( high == -1 ) {
+         this->high = COUNT_HIGH;
+      } else {
+         this->high = high;  
+      }
+      
+      
       this->current_value = 0;
       ledcSetup(channel, this->hz, this->timer_width);
       ledcAttachPin(this->pin, this->channel);
